bc8795b9b99551b720d11f73bd867dd717219150
dh-k22q <97339777+dh-k22q@users.noreply.github.com>
2022-09-14 12:30:43 +0900

    Add files via upload

---
+++ project_soso-mergeTest/src/main/java/com/example/soso/dto/request/CommentRequestDto.java
@@ -0,0 +1,14 @@
+package com.example.soso.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentRequestDto {
+    private Long postId;
+    private String nickname;
+    private String comment;
+}
---
+++ project_soso-mergeTest/src/main/java/com/example/soso/repository/HeartRepository.java
@@ -0,0 +1,13 @@
+package com.example.soso.repository;
+
+import com.example.soso.domain.Heart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface HeartRepository extends JpaRepository<Heart, Long> {
+    Optional<Heart> findByIdAndNickname(Long Id , String nickname);
+    List<Heart> findAllByRequestId(Long RequestID);
+}
